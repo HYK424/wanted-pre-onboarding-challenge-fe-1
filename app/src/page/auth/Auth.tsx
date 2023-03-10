4546f510fae7cf65f9383fabcbb556ac868432cc
HYK424 <turtle424@naver.com>
2023-03-10 18:22:51 +0900

    refactor: 서버 분리

--- app/src/page/auth/Auth.tsx
+++
@@ -1,7 +0,0 @@
-import TabsAuth from "../../components/login/tabs/Tabs";
-
-const Auth = () => {
-  return <TabsAuth />;
-};
-
-export default Auth;
--- app/src/test/TT.tsx
+++
@@ -1,5 +0,0 @@
-const TT = () => {
-  return <div>test</div>;
-};
-
-export default TT;
